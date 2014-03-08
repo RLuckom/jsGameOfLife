9cbbb8a3c46dc6bbc64720235b6f61ad3b119d96
Raphael Luckom <Raphaelluckom@gmail.com>
2014-03-08 13:41:46 -0500

    added play/pause button.

--- src/js/canvasgameoflife.js
+++ src/js/canvasgameoflife.js
@@ -231,131 +231,131 @@ function GOL(unitWidth, unitHeight) {
 
 GOL.prototype = {
 		
-		/**
-		 * Assembles a board unitWidth by unitHeight. All cells initialized 
-		 * to dead.
-		 * 
-		 * @param unitWidth {number}
-		 * @param unitHeight {number}
-		 * @returns {Array}
-		 */
-		makeWorld: function(unitWidth, unitHeight) {
-			var world = [];
-			for (var y = 0; y < unitHeight; y++) {
-				var row = [];
-				for (var x = 0; x < unitWidth; x++) {
-					row.push(this.dead);
-				}
-				world.push(row);
+	/**
+	 * Assembles a board unitWidth by unitHeight. All cells initialized 
+	 * to dead.
+	 * 
+	 * @param unitWidth {number}
+	 * @param unitHeight {number}
+	 * @returns {Array}
+	 */
+	makeWorld: function(unitWidth, unitHeight) {
+		var world = [];
+		for (var y = 0; y < unitHeight; y++) {
+			var row = [];
+			for (var x = 0; x < unitWidth; x++) {
+				row.push(this.dead);
 			}
-			return world;
-		},
-		
-		/**Returns a list of x, y coordinates of the neighbors of point x, y.
-		 * 
-		 * @param {int} x
-		 * @param {int} y
-		 * @return {array} - array of [x, y] arrays.
-		*/
-		getNeighborPoints: function(cell) {
-			var width = this.world[0].length,
-			    height = this.world.length,
-			    neighbors = [];
-			for (var n = 0; n < this.dx.length; n ++) {
-				var new_x = cell.x + this.dx[n],
-				    new_y = cell.y + this.dy[n],
-				    within_x = (0 <= new_x) && (new_x < width),
-				    within_y = (0 <= new_y) && (new_y < height);
-				if (within_x && within_y) {
-					neighbors.push({x: new_x, y: new_y});
-				}
-			}
-			return neighbors;
-		},
-		
-	    /**
-	     * Returns the number of living neighbors cell x, y has.
-	     * 
-	     * @param {int} x
-	     * @param {int} y
-	     * @return {int} - number of living neighbors
-	     */
-		getNumLivingNeighbors: function(cell) {
-			var neighbors = this.getNeighborPoints(cell),
-			    livingNeighbors = 0;
-			
-			// for edge cells, always 0.
-			if (neighbors.length < this.dx.length) {
-				return livingNeighbors;
-			}
-			for (var n = 0; n < neighbors.length; n++) {
-				livingNeighbors += this.world[neighbors[n].y][neighbors[n].x];
-			}
-			return livingNeighbors;
-		},
-		
-		/**
-		 * Determines whether cell is alive or dead this turn.
-		 * 
-		 * @param cell {object} : {x: x, y: y}
-		 * @returns {Number} : this.alive or this.dead.
-		 */
-		decideFate: function(cell) {
-			var livingNeighbors = this.getNumLivingNeighbors(cell);
-			if (this.world[cell.y][cell.x] == this.alive) {
-				for (var n = 0; n < this.survival.length; n++) {
-					if (livingNeighbors == this.survival[n]) {
-						return this.alive;
-					} 
-				}
-			}
-			else if (this.world[cell.y][cell.x] == this.dead) {
-				for (var n = 0; n < this.birth.length; n++) {
-					if (livingNeighbors == this.birth[n]) {
-						return this.alive;
-					} 
-				}				
-			}
-			return this.dead;
-		},
-		
-		/**
-		 * Advances the state of the board one turn.
-		 * 
-		 * @returns {Array} : objects {x: y: alive} x and y in matrix context.
-		 */
-		nextStep: function() {
-			var toggledCells = [];
-			for (var y = 0; y < this.world.length; y++) {
-				for (var x = 0; x < this.world[0].length; x++) {
-					if (this.decideFate({x: x, y: y}) != this.world[y][x]) {
-						toggledCells.push({
-							x: x,
-							y: y,
-							alive: this.world[y][x] ? this.dead : this.alive
-						})
-					}
-				}
-			}
-			for (var n = 0; n < toggledCells.length; n++) {
-				var cell = toggledCells[n];
-				this.world[cell.y][cell.x] = cell.alive;
-			}
-			return toggledCells;
-		},
-		
-		/**
-		 * Switches the state of cell between dead and alive
-		 * 
-		 * @param cell {object} : {x:x, y: y} 
-		 * @returns
-		 */
-		toggle: function(cell) {
-			var y = cell.y, x = cell.x;
-			this.world[y][x] = this.world[y][x] ? this.dead : this.alive;
-			cell.alive = this.world[y][x];
-			return cell;
+			world.push(row);
 		}
+		return world;
+	},
+	
+	/**Returns a list of x, y coordinates of the neighbors of point x, y.
+	 * 
+	 * @param {int} x
+	 * @param {int} y
+	 * @return {array} - array of [x, y] arrays.
+	*/
+	getNeighborPoints: function(cell) {
+		var width = this.world[0].length,
+		    height = this.world.length,
+		    neighbors = [];
+		for (var n = 0; n < this.dx.length; n ++) {
+			var new_x = cell.x + this.dx[n],
+			    new_y = cell.y + this.dy[n],
+			    within_x = (0 <= new_x) && (new_x < width),
+			    within_y = (0 <= new_y) && (new_y < height);
+			if (within_x && within_y) {
+				neighbors.push({x: new_x, y: new_y});
+			}
+		}
+		return neighbors;
+	},
+	
+    /**
+     * Returns the number of living neighbors cell x, y has.
+     * 
+     * @param {int} x
+     * @param {int} y
+     * @return {int} - number of living neighbors
+     */
+	getNumLivingNeighbors: function(cell) {
+		var neighbors = this.getNeighborPoints(cell),
+		    livingNeighbors = 0;
+		
+		// for edge cells, always 0.
+		if (neighbors.length < this.dx.length) {
+			return livingNeighbors;
+		}
+		for (var n = 0; n < neighbors.length; n++) {
+			livingNeighbors += this.world[neighbors[n].y][neighbors[n].x];
+		}
+		return livingNeighbors;
+	},
+	
+	/**
+	 * Determines whether cell is alive or dead this turn.
+	 * 
+	 * @param cell {object} : {x: x, y: y}
+	 * @returns {Number} : this.alive or this.dead.
+	 */
+	decideFate: function(cell) {
+		var livingNeighbors = this.getNumLivingNeighbors(cell);
+		if (this.world[cell.y][cell.x] == this.alive) {
+			for (var n = 0; n < this.survival.length; n++) {
+				if (livingNeighbors == this.survival[n]) {
+					return this.alive;
+				} 
+			}
+		}
+		else if (this.world[cell.y][cell.x] == this.dead) {
+			for (var n = 0; n < this.birth.length; n++) {
+				if (livingNeighbors == this.birth[n]) {
+					return this.alive;
+				} 
+			}				
+		}
+		return this.dead;
+	},
+	
+	/**
+	 * Advances the state of the board one turn.
+	 * 
+	 * @returns {Array} : objects {x: y: alive} x and y in matrix context.
+	 */
+	nextStep: function() {
+		var toggledCells = [];
+		for (var y = 0; y < this.world.length; y++) {
+			for (var x = 0; x < this.world[0].length; x++) {
+				if (this.decideFate({x: x, y: y}) != this.world[y][x]) {
+					toggledCells.push({
+						x: x,
+						y: y,
+						alive: this.world[y][x] ? this.dead : this.alive
+					});
+				}
+			}
+		}
+		for (var n = 0; n < toggledCells.length; n++) {
+			var cell = toggledCells[n];
+			this.world[cell.y][cell.x] = cell.alive;
+		}
+		return toggledCells;
+	},
+	
+	/**
+	 * Switches the state of cell between dead and alive
+	 * 
+	 * @param cell {object} : {x:x, y: y} 
+	 * @returns
+	 */
+	toggle: function(cell) {
+		var y = cell.y, x = cell.x;
+		this.world[y][x] = this.world[y][x] ? this.dead : this.alive;
+		cell.alive = this.world[y][x];
+		return cell;
+	}
 };
 
 /**
@@ -382,9 +382,12 @@ function CanvasGameOfLife(screenWidth, screenHeight, unitWidth, unitHeight) {
 	this.engine = new GOL(unitWidth, unitHeight);
 	this.waitTime = 50;
 	
+	this.mouseIn = false;
+	this.pauseButtonChecked = true;
+	
 	var this_ = this;
 	this.canvas.canvas.addEventListener('click',
-			function(evt) {
+			function (evt) {
 		        var rect = this_.canvas.canvas.getBoundingClientRect(),
 		            point = {x: evt.clientX - rect.left,
 		        	         y: evt.clientY - rect.top};
@@ -393,102 +396,136 @@ function CanvasGameOfLife(screenWidth, screenHeight, unitWidth, unitHeight) {
     this.canvas.canvas.addEventListener("mouseleave", this.makeOnMouseLeave());
     this.canvas.canvas.addEventListener("mouseenter", this.makeOnMouseEnter());
 	this.div = document.createElement('div');
-	this.div.setAttribute('style', 'position: relative;');
-	this.div.appendChild(this.canvas.canvas)
+	var style = "display: block;";
+	this.canvas.canvas.setAttribute('style', style);
+	this.div.setAttribute('style', 'position: relative; display: block;');
+	this.div.appendChild(this.canvas.canvas);
 	this.div.appendChild(this.createSlider());
+	this.div.appendChild(this.createPauseButton());
+	this.runGameOfLife();
 }
 
 CanvasGameOfLife.prototype = {
 
-		/**
-		 * @return {function} - no-argument function that executes 
-		 *                      this.runGameOfLife. 
-		 */
-		makeRunCallback: function () {
-			canvasGOL = this;
-			return function() {
-				canvasGOL.runGameOfLife();
-			};
-		},
-		
-		/**
-		 * @return {function} - no-argument function that sets this.running
-		 *                      to true and runs this.runGameOfLife.
-		 */
-		makeOnMouseLeave: function() {
-			canvasGOL = this;
-			return function() {
-				canvasGOL.running = true;
-				canvasGOL.runGameOfLife();
-			};
-		},
-		
-		/**
-		 * @return {function} - no-argument function that sets this.running
-		 *                      to false.
-		 */
-		makeOnMouseEnter: function() {
-			canvasGOL = this;
-			return function() {
-				canvasGOL.running = false;
-			};
-		},
-		
-		/**
-		 * Runs one step of the GameOfLife if this.running, and sets a timeout
-		 * to run another step.
-		 */
-		runGameOfLife: function () {
-			if (!this.running) {
-				return;
-			}
-			var dirtyCells = this.scale.cellsToSquares(this.engine.nextStep());
-			this.canvas.updateStep(dirtyCells);
-			window.setTimeout(this.makeRunCallback(), this.waitTime);
-		},
-		
-		/**
-		 * Flips the square / cell at canvas x, y, point alive / dead.
-		 * 
-		 * @param point {object} :{x:x, y:y}
-		 */
-		toggleSquare: function(point) {
-			var cell = this.engine.toggle(this.scale.cellFromPoint(point));
-			this.canvas.updateStep([this.scale.cellToSquare(cell)]);
-		},
-		
-		/** creates slider element. */
-		createSlider: function() {
-			var this_ = this;
-		    var sliderInput = document.createElement('input');
-		    sliderInput.setAttribute('type', 'text');
-		    var sliderLabel = document.createElement('label');
-		    sliderLabel.setAttribute('for', 'GoLSlider');
-		    var speedText = "Speed: " + this.waitTime / 1000 + " seconds / step";
-		    sliderLabel.textContent = speedText;
-		    var sliderSpec =  {
-				value: this_.waitTime,
-				min: 0,
-				max: 2000,
-				step: 25,
-				slide: function(event, ui) {
-					this_.waitTime = ui.value;
-					var speed = ui.value / 1000 + " seconds / step";
-					sliderLabel.textContent = "Speed: " + speed;
-				}
-			};
-			var sliderDiv = document.createElement('div');	
-			var labelPara = document.createElement('p');
-		    var slider = document.createElement('div');   
-		    $(slider).slider(sliderSpec);
-			var style = 'position: relative; display: block; width: 200px;';
-		    slider.setAttribute('style', style);
-		    slider.setAttribute('id', 'GoLSlider');
-		    labelPara.appendChild(sliderLabel);
-		    sliderDiv.appendChild(labelPara);
-		    sliderDiv.appendChild(slider);
-		    return sliderDiv;
+	/**
+	 * @return {function} - no-argument function that executes 
+	 *                      this.runGameOfLife. 
+	 */
+	makeRunCallback: function () {
+		canvasGOL = this;
+		return function () {
+			canvasGOL.runGameOfLife();
+		};
+	},
+	
+	/**
+	 * @return {function} - no-argument function that sets this.running
+	 *                      to true and runs this.runGameOfLife.
+	 */
+	makeOnMouseLeave: function () {
+		canvasGOL = this;
+		return function () {
+			canvasGOL.mouseIn = false;
+			canvasGOL.runGameOfLife();
+		};
+	},
+	
+	/**
+	 * @return {function} - no-argument function that sets this.running
+	 *                      to false.
+	 */
+	makeOnMouseEnter: function () {
+		canvasGOL = this;
+		return function () {
+			canvasGOL.mouseIn = true;
+		};
+	},
+	
+	/**
+	 * Runs one step of the GameOfLife if this.running, and sets a timeout
+	 * to run another step.
+	 */
+	runGameOfLife: function () {
+		if (this.mouseIn || this.pauseButtonChecked) {
+			return;
 		}
+		var dirtyCells = this.scale.cellsToSquares(this.engine.nextStep());
+		this.canvas.updateStep(dirtyCells);
+		window.setTimeout(this.makeRunCallback(), this.waitTime);
+	},
+	
+	/**
+	 * Flips the square / cell at canvas x, y, point alive / dead.
+	 * 
+	 * @param point {object} :{x:x, y:y}
+	 */
+	toggleSquare: function (point) {
+		var cell = this.engine.toggle(this.scale.cellFromPoint(point));
+		this.canvas.updateStep([this.scale.cellToSquare(cell)]);
+	},
+	
+	/** creates slider element. */
+	createSlider: function () {
+		var this_ = this;
+	    var sliderInput = document.createElement('input');
+	    sliderInput.setAttribute('type', 'text');
+	    var sliderLabel = document.createElement('label');
+	    sliderLabel.setAttribute('for', 'GoLSlider');
+	    var speedText = "Speed: " + this.waitTime / 1000 + " seconds / step";
+	    sliderLabel.textContent = speedText;
+	    var sliderSpec =  {
+			value: this_.waitTime,
+			min: 0,
+			max: 2000,
+			step: 25,
+			slide: function (event, ui) {
+				this_.waitTime = ui.value;
+				var speed = ui.value / 1000 + " seconds / step";
+				sliderLabel.textContent = "Speed: " + speed;
+			}
+		};
+		var sliderDiv = document.createElement('div');	
+		var labelPara = document.createElement('p');
+	    var slider = document.createElement('div');   
+	    $(slider).slider(sliderSpec);
+		var style = 'position: static; display: inline-block; width: 200px;';
+	    sliderDiv.setAttribute('style', style);
+	    slider.setAttribute('id', 'GoLSlider');
+	    labelPara.appendChild(sliderLabel);
+	    sliderDiv.appendChild(labelPara);
+	    sliderDiv.appendChild(slider);
+	    return sliderDiv;
+	},
+	
+	createPauseButton: function () {
+		var button = document.createElement("input");
+		var label = document.createElement("label");
+		var pauseButtonDiv = document.createElement('div');
+		var style = 'position: relative; display: inline-block; width: 200px; margin: 20px;';
+		
+		button.setAttribute("type", "checkbox");
+		pauseButtonDiv.setAttribute('style', style);
+		button.setAttribute("id", "pauseButton");
+		label.setAttribute('for', "pauseButton");
+		label.textContent = "Play";
+		var this_ = this;
+		$(button).change(function () {
+			if (this.checked) {
+				this_.pauseButtonChecked = false;
+				$(button).button("option", "label", 'Pause');
+				this_.runGameOfLife();
+			}
+			else {
+				this_.pauseButtonChecked = true;
+				$(button).button("option", "label", 'Play');
+			}
+		});
+		pauseButtonDiv.appendChild(button);
+	    pauseButtonDiv.appendChild(label);
+		//pauseButtonDiv.setAttribute('style', style);
+		$(button).button();
+		return pauseButtonDiv;
+	}
 };
 
 /**
